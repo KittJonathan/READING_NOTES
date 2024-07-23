66e9edf9a222a37719665619abb075e6eaad005b
Jonathan Kitt <jonathan.kitt@inrae.fr>
2024-07-23 08:46:08 +0200

    working on chapter 2

--- STATISTICS_IN_NATURAL_RESOURCES/NOTES.R
+++ STATISTICS_IN_NATURAL_RESOURCES/NOTES.R
@@ -5,8 +5,10 @@
 
 # PACKAGES ----------------------------------------------------------------
 
+library(stats4nr)
 library(tidyverse)
 library(hexbin)
+library(modeest)
 
 # 1. VISUALIZING DATA -----------------------------------------------------
 
@@ -182,3 +184,89 @@ ggplot(data = elm,
   geom_point() +
   scale_x_continuous(limits = c(10, 15)) +
   scale_y_continuous(limits = c(30, 40))
+
+# 2. SUMMARY STATISTICS AND DISTRIBUTIONS ---------------------------------
+
+head(chirps)
+
+chirps |> 
+  arrange(chirps)
+
+# Measures of central tendency
+mean(chirps$cps)
+median(chirps$cps)
+mfv(chirps$cps)
+
+# Measures of spread
+var(chirps$cps)
+sd(chirps$cps)
+
+(sd(chirps$cps) / mean(chirps$cps)) * 100  # coeff of variation (CV)
+
+# Measures of position (z scores)
+(16 - mean(chirps$cps)) / sd(chirps$cps)
+(20 - mean(chirps$cps)) / sd(chirps$cps)
+
+ggplot(data = chirps, 
+       aes(x = 1, y = cps)) +
+  geom_boxplot() +
+  scale_x_continuous(breaks = NULL) +
+  labs(y = "Chirps per second")
+
+head(ant)
+
+ant |> 
+  summarise(spprich_mean = mean(spprich),
+            .by = ecotype)
+
+ant |> 
+  ggplot(aes(x = ecotype, y = spprich)) +
+  geom_violin()
+
+ant |> 
+  reframe(spprich_range = range(spprich),
+          .by = ecotype)
+
+ant |> 
+  summarise(spprich_iqr = IQR(spprich),
+            .by = ecotype)
+
+ant |> 
+  summarise(spprich_sd = sd(spprich),
+            spprich_cv = 100 * sd(spprich) / mean(spprich),
+            .by = ecotype)
+
+ant |> 
+  summarise(spprich_sd = sd(spprich),
+            spprich_mean = mean(spprich),
+            .by = ecotype) |> 
+  mutate(x = 7,
+         z = (x - spprich_mean) / spprich_sd)
+
+z_score <- function(x_i, pop_mean, pop_sd) {
+  
+  z <- (x_i - pop_mean) / pop_sd
+  return(z)
+
+  }
+
+z_score(x_i = 38, pop_mean = 42, pop_sd = 3)
+
+deer <- tibble(
+  deer = c(0, 1, 2),
+  prob = c(0.7, 0.1, 0.2))
+
+deer_mu <- sum(deer$deer * deer$prob)
+deer_sd <- sqrt(sum((deer$deer - deer_mu)^2 * deer$prob))
+
+wolves <- tibble(
+  age = 0:9,
+  num_wolves = c(10, 9, 21, 8, 9, 5, 2, 2, 1, 2))
+
+wolves |> 
+  summarise(age_mean = mean(age),
+            age_sd = sd(age))
+
+summary(wolves$age)
+
+z_score(x_i = 6, pop_mean = 4.5, pop_sd = 3.03)
