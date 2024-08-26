e62dfd4991a6ac8459914a8fc916c141ea196ec2
Jonathan Kitt <jonathan.kitt@inrae.fr>
2024-08-26 16:49:09 +0200

    start reading Applied statistics with R

---
+++ APPLIED_STATISTICS_WITH_R/NOTES.R
@@ -0,0 +1,10 @@
+# APPLIED STATISTICS WITH R
+# DAVID DALPIAZ
+# 2024-08-26
+# https://book.stat420.org/
+
+# 1. INTRODUCTION ---------------------------------------------------------
+
+# 2. INTRODUCTION TO R ----------------------------------------------------
+
+
