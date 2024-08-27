bf0b9c1856f67497f0777c3693287b8747ed0b23
Jonathan Kitt <jonathan.kitt@inrae.fr>
2024-08-27 09:58:34 +0200

    -> chapter 3

--- APPLIED_STATISTICS_WITH_R/NOTES.R
+++ APPLIED_STATISTICS_WITH_R/NOTES.R
@@ -7,4 +7,49 @@
 
 # 2. INTRODUCTION TO R ----------------------------------------------------
 
+## 2.1. GETTING STARTED ---------------------------------------------------
+
+# Alt + Shift + K -> keyboard shortcuts
+
+## 2.2. BASIC CALCULATIONS ------------------------------------------------
+
+# Addition, substraction, multiplication, and division
+3 + 2
+3 - 2
+3 * 2
+3 / 2
+
+# Exponents
+3 ^ 2
+2 ^ (-3)
+100 ^ (1 / 2)
+sqrt(100)
+
+# Mathematical constants
+pi
+exp(1)
+
+# Logarithms
+log(exp(1))
+log10(1000)
+log2(8)
+log(16, base = 4)
+
+# Trigonometry
+sin(pi / 2)
+cos(0)
+
+## 2.3. GETTING HELP ------------------------------------------------------
+
+?log
+?sin
+?paste
+?lm
+
+# 2.4. INSTALLING PACKAGES ------------------------------------------------
+
+# install.packages("ggplot2")
+# library(ggplot2)
+
+# 3. DATA AND PROGRAMMING -------------------------------------------------
 
