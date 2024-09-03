85018209532d6f719e23bf26c63a126722fd0f8f
Jonathan Kitt <jonathan.kitt@inrae.fr>
2024-09-03 11:20:42 +0200

    Practical statistics in medicine with R -> ch3

---
+++ PRACTICAL_STATISTICS_IN_MEDICINE_WITH_R/NOTES.R
@@ -0,0 +1,53 @@
+# PRACTICAL STATISTICS IN MEDICINE WITH R
+# KONSTANTINOS I. BOUGIOUKAS
+# 2024-09-03
+# https://practical-stats-med-r.netlify.app/
+
+# 1. R VIA RSTUDIO --------------------------------------------------------
+
+# https://practical-stats-med-r.netlify.app/intro_rstudio
+
+## 1.1. INSTALLING R AND RSTUDIO ------------------------------------------
+
+## 1.2. STARTING R & RSTUDIO ----------------------------------------------
+
+14 + 16
+
+## 1.3. ERRORS, WARNINGS, AND MESSAGES IN R -------------------------------
+
+hello
+# 14 + 16 - 
+
+# 1.4. R HELP RESOURCES ---------------------------------------------------
+
+help(median)
+?median
+
+help.search("geometric median")
+??"geometric median"
+
+apropos("med")
+
+example(median)
+
+# 2. RSTUDIO PROJECTS -----------------------------------------------------
+
+## 2.1. WORKING WITH RSTUDIO PROJECTS -------------------------------------
+
+### 2.1.1. CREATING AN RSTUDIO PROJECT ------------------------------------
+
+### 2.1.2. ORGANIZING OUR RSTUDIO PROJECT FOLDER --------------------------
+
+# data/
+# figures/
+
+## 2.2. OPENING A NEW R SCRIPT --------------------------------------------
+
+## 2.3. ADDING COMMENTS TO A SCRIPT ---------------------------------------
+
+14 + 16  # add two integers
+# 45.3 + 20.5  # add two decimals
+
+# 3. R AS CALCULATOR ------------------------------------------------------
+
+
